1a0d09cbd931149861424b9401c0bbe99fb4e6aa
hayani said <hayanisaid1995@gmail.com>
2018-02-17 20:04:58 +0000

    add form contact section and styles

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
  $('.header').height($(window).height());
 
+ $(".navbar a").click(function(){
+ 	$("body,html").animate({
+ 		scrollTop:$("#" + $(this).data('value')).offset().top
+ 	},1000)
+  
+ })
+
 })
